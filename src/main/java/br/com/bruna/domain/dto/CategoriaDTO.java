799269ba39549f45681d6e1269ec4df78add45c1
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-13 20:19:44 -0300

    Listando todas as categorias com DTO

---
+++ src/main/java/br/com/bruna/domain/dto/CategoriaDTO.java
@@ -0,0 +1,41 @@
+package br.com.bruna.domain.dto;
+
+import java.io.Serializable;
+
+import br.com.bruna.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	
+	public CategoriaDTO() {
+		
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setName(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
