19329d72475bfc5b5266cfa217f596da7e81eadf
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-17 15:49:56 -0300

    Validação sintática com Bean Validation

--- src/main/java/br/com/bruna/domain/dto/CategoriaDTO.java
+++ src/main/java/br/com/bruna/domain/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package br.com.bruna.domain.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.bruna.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -9,6 +13,8 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	@NotEmpty(message="O preenchimento do nome da categoria é obrigatório")
+	@Length(min=5, max=80, message="O nome da categoria deve conter entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/br/com/bruna/services/CategoriaService.java
+++ src/main/java/br/com/bruna/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.bruna.domain.Categoria;
+import br.com.bruna.domain.dto.CategoriaDTO;
 import br.com.bruna.repositories.CategoriaRepository;
 import br.com.bruna.services.exceptions.DataIntegrityException;
 import br.com.bruna.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findByPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDto(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
