31b3b1d657da77278f746f6a17845141cb09f839
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-20 21:37:15 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/br/com/bruna/domain/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package br.com.bruna.domain.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.bruna.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	@NotEmpty(message = "O preenchimento do nome é obrigatório")
+	@Length(min = 5, max = 150, message = "O nome deve conter entre 5 e 150 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "O preenchimento do nome é obrigatório")
+	@Email(message = "Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
--- src/main/java/br/com/bruna/services/ClienteService.java
+++ src/main/java/br/com/bruna/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.bruna.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.bruna.domain.Cliente;
+import br.com.bruna.domain.dto.ClienteDTO;
 import br.com.bruna.repositories.ClienteRepository;
+import br.com.bruna.services.exceptions.DataIntegrityException;
 import br.com.bruna.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +28,37 @@ public class ClienteService {
 				"Objeto não encontrado. ID: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui registros");
+		}
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Page<Cliente> findByPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
 }
