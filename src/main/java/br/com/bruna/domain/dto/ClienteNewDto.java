a9cdd7c206a23eec269e31977f482906cdc6eed2
Bruna Gonçalves <brunabkg2@gmail.com>
2020-05-25 19:50:22 -0300

    Validação customizada : CPF ou CNPJ na inserção de cliente

--- src/main/java/br/com/bruna/domain/dto/ClienteNewDto.java
+++ src/main/java/br/com/bruna/domain/dto/ClienteNewDto.java
@@ -2,22 +2,45 @@ package br.com.bruna.domain.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.bruna.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDto implements Serializable{
 
 
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "O preenchimento do nome é obrigatório")
+	@Length(min = 5, max = 150, message = "O nome deve conter entre 5 e 150 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "O preenchimento do email é obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
+	
+	@NotEmpty(message = "O preenchimento do CPF / CNPJ é obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message = "O preenchimento do logradouro é obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message = "O preenchimento do número é obrigatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "O preenchimento do CEP é obrigatório")
 	private String cep;
 	
+	@NotEmpty(message = "O preenchimento do telefone é obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
---
+++ src/main/java/br/com/bruna/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package br.com.bruna.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import br.com.bruna.domain.dto.ClienteNewDto;
+import br.com.bruna.domain.enums.TipoCliente;
+import br.com.bruna.resources.exceptions.FieldMessage;
+import br.com.bruna.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDto> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDto objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
