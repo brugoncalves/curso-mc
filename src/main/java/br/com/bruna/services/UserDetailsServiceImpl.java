dc2f0402364e84ef6d82be8b20d5e05e4aac7ca3
Bruna Gonçalves <brunabkg2@gmail.com>
2020-08-23 12:27:35 -0300

    Implementando autenticação e geração do token JWT

--- src/main/java/br/com/bruna/services/UserDetailsServiceImpl.java
+++ src/main/java/br/com/bruna/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import br.com.bruna.domain.Cliente;
 import br.com.bruna.repositories.ClienteRepository;
 import br.com.bruna.security.UserSs;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
