bc7ef8613c15541510c80e32601f1cf985ee8865
Bruna Gonçalves <brunabkg2@gmail.com>
2020-09-09 20:09:02 -0300

    Esqueci minha senha

--- src/main/java/br/com/bruna/resources/AuthResource.java
+++ src/main/java/br/com/bruna/resources/AuthResource.java
@@ -1,20 +1,27 @@
 package br.com.bruna.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.bruna.domain.dto.EmailDTO;
 import br.com.bruna.security.JWTUtil;
 import br.com.bruna.security.UserSs;
+import br.com.bruna.services.AuthService;
 import br.com.bruna.services.UserService;
 
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthResource {
+	
+	@Autowired
+	private AuthService authService;
 
 	@Autowired
 	private JWTUtil jwtUtil;
@@ -24,6 +31,12 @@ public class AuthResource {
 		UserSs user = UserService.authenticated();
 		String token = jwtUtil.generationToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping(value = "/forgot")
+	public ResponseEntity<Void> forgotPassword(@RequestBody @Valid EmailDTO objDto){
+		authService.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
